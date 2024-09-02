14331fe5916e0e986d48af4f1b0ef97fbc25ff00
md1300 <md.tawhid.khan1998@gmail.com>
2024-09-03 03:46:54 +0600

    setup express node js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const cors=require('cors')
+const app=express()
+
+const port=4000
+
+app.use(cors())
+
+
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
+app.listen(port,()=>{
+    console.log(`the running server is ${port}`)
+})
