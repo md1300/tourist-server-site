b551cfcd5b23567c09bde98adbaeceb5347ed623
md1300 <md.tawhid.khan1998@gmail.com>
2024-09-08 02:57:44 +0600

    hide db password and usernme using dotenv

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express')
 const cors=require('cors')
+require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
 const app=express()
 
@@ -11,14 +12,8 @@ app.use(express.json())
 
 
 
-// -------------------------------------------
-// userName:CountriesCollection
-// password:1qE2wdGByQQNAXqK
-// --------------------------------------------
-
-
-
-const uri = "mongodb+srv://CountriesCollection:1qE2wdGByQQNAXqK@cluster0.vmhty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vmhty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
